9f8d758563afe8a34e3f56ce3397ac2100ae235e
zy <1165045817@qq.com>
2019-08-25 23:46:55 +0800

    界面完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
+import Home from '@/views/home'
+import NotFound from '@/views/404'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-
-    { path: '/login', name: 'login', component: Login }
+    { path: '/login', name: 'login', component: Login },
+    { path: '/home', name: 'home', component: Home },
+    { path: '*', name: '404', component: NotFound }
   ]
 })
 
